4d5b0c0d588bb574aaadfe8a621dcb7681164727
XingHongChenIntel <xinghong.chen@intel.com>
2021-02-01 18:37:26 +0800

    clean the code.

--- pmlc/conversion/tile_to_pxa/tile_to_pxa.cc
+++ pmlc/conversion/tile_to_pxa/tile_to_pxa.cc
@@ -1963,7 +1963,6 @@ struct ScfForOpConversion : public OpConversionPattern<scf::ForOp> {
     for (unsigned i = 0; i < oldArgs.size(); ++i) {
       oldArgs[i].replaceAllUsesWith(newArgs[i]);
     }
-    newOp.dump();
     rewriter.replaceOp(op, newOp.results());
     return success();
   }
